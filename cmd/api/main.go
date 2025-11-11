279482af76aaa390c5c30d6ad8b552dc4c131bed
TORIOP <toriop.dev@gmail.com>
2025-11-11 14:43:32 +0700

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"example.com/goapi/internal/handlers"
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.SetReportCaller(true)
+	var r *chi.Mux = chi.NewRouter()
+	handlers.Handler(r)
+	fmt.Println("Starting GO API service...")
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Error(err)
+	}
+}
